295e5b9d47434884587a179e2d5d4a47cdc74629
Michael Christenson <mchristenson5891@gmail.com>
2019-05-10 16:43:22 -0700

    add login route

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
           <Route exact path={routes.USERS} render={() => <div>USER</div>} />
           <Route exact path={`${routes.USERS}/:id`} render={() => <ShowUser />} />
           <Route exact path={routes.POSTS} render={() => <div>POST</div>} />
-          <Route exact path={'/login'} render={() => <Login currentUser={this.state.currentUser} doSetCurrentUser={this.doSetCurrentUser}/>} />
+          <Route exact path={routes.LOGIN} render={() => <Login currentUser={this.state.currentUser} doSetCurrentUser={this.doSetCurrentUser}/>} />
           <Route render={() => <div>NOT FOUND</div>} />
         </Switch>
       </div>
